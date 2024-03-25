9f1214fc62f386d23d59fdb914931547527073b5
DESKTOP-EBP024J\ASUS <tunnph25693@fpt.edu.vn>
2024-03-25 09:01:54 +0700

    Update

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  constructor(private elementRef: ElementRef) {}
+
+  scrollToElement(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+  }
+}
